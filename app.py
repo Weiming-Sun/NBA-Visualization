f6a76f05e82049c4c162a5b70b95c5150ff32058
Weiming-Sun <30918347+Weiming-Sun@users.noreply.github.com>
2018-03-24 23:59:33 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, jsonify
 
 df = pd.read_csv('NBA.csv')
 
+colorlist = []
+for team in df["TEAM"]:
+    if team == "GSW":
+        colorlist.append("rgb(0, 107, 182)")
+    else:
+        colorlist.append("rgb(255, 0, 0)")
+df['COLOR'] = colorlist
+
+df["text"] = df["PLAYER"] + " (" + df["TEAM"] + ")"
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,15 +24,25 @@ def home():
 def nbasalary(year):
 
     tempdf = df[df['Season']==int(year)]
+    tempdf = tempdf.reset_index(drop=True)
+    '''tempdf.to_csv('result.csv')'''
 
     x = tempdf['SAL'].values.tolist()
     y = tempdf['+/-'].values.tolist()
-    text = tempdf['PLAYER'].values.tolist()    
+    text = tempdf['text'].values.tolist()    
+    color = tempdf['COLOR'].values.tolist()   
     size = (tempdf['PTS']/35).values.tolist()
 
-    bubbledata = {"x": x, "y": y, "hovertext": text, "mode": "markers", "marker": {"color": "rgb(255, 0, 0)", "size": size}}
+    data = {"x": x, "y": y, "hovertext": text, "mode": "markers", "marker": {"color": color, "size": size}}
 
-    return jsonify(bubbledata)
+    tempdf = df.iloc[0:0]
+    x = []
+    y = []
+    text = []
+    color = []
+    size = []
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
